54b05c07fa544c341064475b8b192c3494af08d5
aniketbisht24 <aniketbisht24@gmail.com>
2022-05-29 20:09:01 +0530

    first_project done successfully

---
+++ lib/pages/loading.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:first_project/services/time-location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setUpWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'London', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpWorldTime();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitCubeGrid(
+          color: Colors.white,
+          size: 80,
+        ),
+      ),
+    );
+  }
+}
